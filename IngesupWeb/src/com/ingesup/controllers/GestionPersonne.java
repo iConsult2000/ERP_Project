dd9ccfea014eb6e17ddc7f0777e0efd3c89bde14
Steeve DACALOR <M_S_DACALOR@YAHOO.FR>
2012-04-19 18:10:28 +0200

    todo GestionPersonne

--- IngesupWeb/src/com/ingesup/controllers/GestionPersonne.java
+++ IngesupWeb/src/com/ingesup/controllers/GestionPersonne.java
@@ -1,23 +1,21 @@
 package com.ingesup.controllers;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class Authentification
+ * Servlet implementation class GestionPersonne
  */
-public class Authentification extends HttpServlet {
+public class GestionPersonne extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Authentification() {
+    public GestionPersonne() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,17 +32,6 @@ public class Authentification extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		HttpSession session = request.getSession(true);
-		
-		//ajout dans la session
-		session.setAttribute("id", request.getParameter("id"));
-		session.setAttribute("mdp", request.getParameter("mdp"));
-		
-		//type
-		session.setAttribute("type", "service_pedagogique");
-		
-		//redirection
-		request.getRequestDispatcher("/").forward(request, response);
 	}
 
 }
