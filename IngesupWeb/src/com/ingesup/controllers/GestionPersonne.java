68c915dc35e2b7cbbe45f11a493e0ef536c29d24
phejar <manuel@phejar.fr>
2012-04-20 14:56:10 +0200

    gestion personne

--- IngesupWeb/src/com/ingesup/controllers/GestionPersonne.java
+++ IngesupWeb/src/com/ingesup/controllers/GestionPersonne.java
@@ -1,12 +1,17 @@
 package com.ingesup.controllers;
 
 import java.io.IOException;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ingesup.beans.facade.ejb.Local.GestionSvePdeLocal;
+import com.ingesup.beans.persistence.Adresse;
+import com.ingesup.beans.persistence.Etudiant;
+import com.ingesup.beans.persistence.Personne;
 
 /**
  * Servlet implementation class GestionPersonne
@@ -14,6 +19,7 @@ import com.ingesup.beans.facade.ejb.Local.GestionSvePdeLocal;
 public class GestionPersonne extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	@EJB GestionSvePdeLocal gestionSvePdeLocal;
 	
     /**
      * @see HttpServlet#HttpServlet()
@@ -35,6 +41,21 @@ public class GestionPersonne extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		String nom = request.getParameter("nom");
+		String prenom = request.getParameter("prenom");
+		String email = request.getParameter("email");
+		
+		Adresse adresse = new Adresse("7 Rue de la mort", "Enfer", 777);
+		
+		Etudiant e = new Etudiant(nom, prenom, email, adresse, "0631323135", 1, 1);
+		
+		gestionSvePdeLocal.addEtudiant(e);
+		
+		System.out.println("L'étudiant "+nom+" "+prenom+" a bien été ajouté!");
+		
+		//redirection
+		request.getRequestDispatcher("/").forward(request, response);
 	}
 
 }
