35119453f26ae0640759744d5e2be3263498303f
Steeve DACALOR <M_S_DACALOR@YAHOO.FR>
2012-04-20 16:43:01 +0200

    Etudiant Remote

--- IngesupEJB/ejbModule/com/ingesup/beans/facade/ejb/GestionEtudiantBean.java
+++ IngesupEJB/ejbModule/com/ingesup/beans/facade/ejb/GestionEtudiantBean.java
@@ -4,8 +4,12 @@ import java.io.Serializable;
 
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NamedQuery;
+import javax.persistence.PersistenceContext;
 
 import com.ingesup.beans.facade.ejb.Remote.GestionEtudiantRemote;
+import com.ingesup.beans.persistence.Classe;
 
 /**
  * Session Bean implementation class GestionEtudiant
@@ -14,11 +18,20 @@ import com.ingesup.beans.facade.ejb.Remote.GestionEtudiantRemote;
 @Remote(GestionEtudiantRemote.class)
 public class GestionEtudiantBean implements GestionEtudiantRemote, Serializable {
 
+	@PersistenceContext EntityManager em;
     /**
      * Default constructor. 
      */
     public GestionEtudiantBean() {
-        // TODO Auto-generated constructor stub
+        
+    }
+    
+    /**
+     * Return the agenda number for a school
+     */
+    public String getNumAgenda(int idClasse) {
+    	Classe q = em.find(Classe.class, idClasse);
+    	return q.getNo_agenda();
     }
 
 }
--- IngesupEJB/ejbModule/com/ingesup/beans/facade/ejb/Remote/GestionEtudiantRemote.java
+++ IngesupEJB/ejbModule/com/ingesup/beans/facade/ejb/Remote/GestionEtudiantRemote.java
@@ -1,7 +1,10 @@
 package com.ingesup.beans.facade.ejb.Remote;
+import javax.annotation.security.PermitAll;
 import javax.ejb.Remote;
 
 @Remote
 public interface GestionEtudiantRemote {
 	
+	@PermitAll
+	public String getNumAgenda(int idClasse);
 }
