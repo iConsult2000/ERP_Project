fe74f3ac664287d888ffdcb920737089bd3ca1e2
Lory97 <vermine37@gmail.com>
2012-05-08 04:55:01 +0200

    recherche 100% update 50% delete 0%

---
+++ IngesupWeb/WebContent/views/service_pedagogique/update_event.jsp
@@ -0,0 +1,71 @@
+<!-- Tableau obligatoire ! C'est lui qui contiendra le calendrier ! -->
+<table class="ds_box" cellpadding="0" cellspacing="0" id="ds_conclass" style="display: none;">
+	<tr>
+		<td id="ds_calclass"></td>
+	</tr>
+</table>
+
+<% 
+String datedebut="";
+String datefin="";
+String leTitre="";
+String heuredebut="";
+String heurefin="";
+
+if (session.getAttribute("evt") != null) {
+		 datedebut =  (String)session.getAttribute("datedebut");
+		 datefin = (String)session.getAttribute("datefin");
+		 leTitre = (String)session.getAttribute("titre");
+		 heuredebut = (String)session.getAttribute("heuredebut");
+		 heurefin =  (String)session.getAttribute("heurefin");
+}
+		%>
+
+<div id="formulaire_upd_evenement">
+	<form method="post" action="update">
+	<table class="formulaire_evenement">
+	<tr>
+		<td class="titre"><p>titre</p></td>
+		<td colspan="3"><input type="text" name="titre" class="titre" value="<%=leTitre%>"/></td>
+	</tr>
+	<tr>
+		<td class="date"><p>De</p></td>
+		<td><input type="text" name="date1" onclick="ds_sh(this);" class="date" value="<%=datedebut%>"/>
+			<input type="text" name="heure1" value="<%=heuredebut%>" 
+			onfocus="if(this.value == '00:00') { this.value = ''; }"
+			onblur="if(this.value == '') { this.value = '00:00'; }" class="heure" /></td>
+		<td><p>A </p></td>
+		<td>
+			<input type="text" name="date2" onclick="ds_sh(this);" class="date" value="<%=datefin %>"/>
+			<input type="text" name="heure2" value="<%=heurefin%>"
+			onfocus="if(this.value == '00:00') { this.value = ''; }"
+			onblur="if(this.value == '') { this.value = '00:00'; }" class="heure" /></td>
+	</tr>
+	<tr>
+		<td class="detail"><p>Détail</p></td>
+		<td colspan="3" align="right"><table class="even">		
+				<tr>
+					<td class="lieu"><p>Lieu </p></td>
+					<td><input type="text" class="lieu" name="lieu" /></td>
+				</tr>
+				<tr>
+					<td class="cour"><p>Cour </p></td>
+					<td><select class="cour"><option></option></select></td>
+				</tr>
+				<tr>
+					<td class="desc"><p>Description </p></td>
+					<td><textarea class="desc" rows="3" name="desc"></textarea></td>
+				</tr>
+			</table></td>
+	</tr>
+	<tr>
+		<td class="couleur"><p>Couleur</p></td>
+		<td colspan="3"> </td>
+	</tr>
+	<tr>
+		<td colspan="4" align="right"><input type="submit" value="valider" /></td>
+	</tr>
+	</table>
+	</form>
+	<form class="close"><input type="button" value="fermer" onClick="hide_even();ds_hi()" /></form>
+</div>
--- IngesupWeb/src/com/ingesup/controllers/CalendarController.java
+++ IngesupWeb/src/com/ingesup/controllers/CalendarController.java
@@ -70,30 +70,89 @@ public class CalendarController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
 		
 		client = new CalendarService("API Project");
 		try {
 			client.setUserCredentials("erpingesuparis@gmail.com", "iConsult2000");
 		} catch (AuthenticationException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		if (getInitParameter("operation").equals("ajouter")) addEvt(request,response);
 		if (getInitParameter("operation").equals("modifier")) updateEvt(request,response);
 		if (getInitParameter("operation").equals("supprimer")) deletEvt(request,response);
+		if (getInitParameter("operation").equals("chercher")) searchEvt(request,response);
+		if (getInitParameter("operation").equals("getEvt")) getEvt(request,response);
 		
 		
-		request.getRequestDispatcher("/").forward(request, response);
-		
+		request.getRequestDispatcher("/").forward(request, response);		
 		
 	}
 	
+	private void getEvt(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void searchEvt(HttpServletRequest request,HttpServletResponse response) {
+		
+		String  datedebut = normalizeDate(request.getParameter("date1"),"00:00");
+		String  datefin = normalizeDate(request.getParameter("date2"),"23:59");
+		String titre = request.getParameter("titre");
+		
+		//L'url ou les données sont stockées.
+		URL feedUrl = null;
+		try {
+			feedUrl = new URL("https://www.google.com/calendar/feeds/"+SIGL2+"/private/full");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// requête pour récupérer des données évènement.
+		CalendarQuery myQuery = new CalendarQuery(feedUrl);
+		
+		if(titre!=""){
+			myQuery.setFullTextQuery(titre);
+
+		}
+		
+		if(datedebut!="" && datefin!="" ){
+		// les évènements qui se sont passés entre date1 et date2.
+		myQuery.setMinimumStartTime(DateTime.parseDateTime(datedebut));
+		myQuery.setMaximumStartTime(DateTime.parseDateTime(datefin));
+		}
+		
+		CalendarEventFeed resultFeed = null;
+		try {
+			resultFeed = client.query(myQuery, CalendarEventFeed.class);//on execute la requête
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ServiceException e) {
+			e.printStackTrace();
+		}	
+		//on récupere les résultats.
+		List<CalendarEventEntry> lstEvent = resultFeed.getEntries();
+		
+		for (int i = 0; i < lstEvent.size();i++){
+			CalendarEventEntry entry = (CalendarEventEntry)lstEvent.get(i);
+			//on affiche le titre de l'évenement
+			System.out.println("titre évènement n°"+i+":"+entry.getTitle().getPlainText());
+			//on affiche la description de l'évènement.
+			System.out.println("description évènement n°"+i+":"+entry.getPlainTextContent());
+			System.out.println(""+entry.getTimes().get(0).getStartTime().toUiString());
+			System.out.println(""+entry.getTimes().get(0).getEndTime().toUiString());
+		}
+		
+		HttpSession session = request.getSession();
+		session.setAttribute("listEvt", lstEvent);
+		
+		
+		
+	}
+
 	private void addEvt(HttpServletRequest request, HttpServletResponse response) throws IOException
 	{
-		
 		//L'url ou les données sont stockées.
 		URL feedUrl = null;
 		try 
@@ -114,12 +173,9 @@ public class CalendarController extends HttpServlet {
 		String heurefin =  request.getParameter("heure2");
 		
 		String description = request.getParameter("desc");
-		
-		String[] rawDate = datedebut.split("/");
-		datedebut = rawDate[2]+"-"+rawDate[1]+"-"+rawDate[0]+"T"+heuredebut+":00";
-		
-		rawDate = datefin.split("/");
-		datefin = rawDate[2]+"-"+rawDate[1]+"-"+rawDate[0]+"T"+heurefin+":00";
+	
+		datedebut = normalizeDate(datedebut,heuredebut);
+		datefin = normalizeDate(datefin,heurefin);
 		
 		System.out.println("date de début :"+datedebut+"    date de fin :"+datefin);
 		
@@ -144,6 +200,18 @@ public class CalendarController extends HttpServlet {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	
+	private String normalizeDate(String date,String heure)
+	{
+		System.out.println(date);
+		
+		if(date!="" && heure!=""){
+		String[] rawDate = date.split("/");
+		return rawDate[2]+"-"+rawDate[1]+"-"+rawDate[0]+"T"+heure+":00";
+		}
+		else return "";
 	}
 	
 	private void updateEvt(HttpServletRequest request, HttpServletResponse response)
